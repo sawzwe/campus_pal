3255f654db2ec1f3ec001aaf7a87cfa87f883d4d
sawzwe26 <sawjohnwaiyan@gmail.com>
2023-08-29 17:25:39 +0700

    Draft Page Routes

---
+++ app/admin/Booking/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Client Booking Details Page
+    </div>
+  )
+}
+
+export default page
---
+++ app/client/Category/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Client Category Page
+    </div>
+  )
+}
+
+export default page
