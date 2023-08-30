93fa8d66565bc3c0c765af85a703bcceefeb5068
sawzwe26 <sawjohnwaiyan@gmail.com>
2023-08-30 18:24:20 +0700

    update

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -1,42 +1,42 @@
-import NextAuth from "next-auth/next";
-import prisma from "../../../libs/prismadb";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
+// import NextAuth from "next-auth/next";
+// import prisma from "../../../libs/prismadb";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import GoogleProvider from "next-auth/providers/google";
+// import GithubProvider from "next-auth/providers/github";
 
-export const authOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    CredentialsProvider({
-        name: "credentials",
-        credentials: {
-            email : {label: "Email", type: "text", placeholder: "pork@gmail.com"},
-            password : {label: "Password", type: "password"},
-            username:{label: "Username", type: "text", placeholder:"john pork"},
-        },
-        async authorize(credentials) {
-            const user= {id: 1 , name: "John Pork", email: "johnpork@gmail.com"}
+// export const authOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET,
+//     }),
+//     CredentialsProvider({
+//         name: "credentials",
+//         credentials: {
+//             email : {label: "Email", type: "text", placeholder: "pork@gmail.com"},
+//             password : {label: "Password", type: "password"},
+//             username:{label: "Username", type: "text", placeholder:"john pork"},
+//         },
+//         async authorize(credentials) {
+//             const user= {id: 1 , name: "John Pork", email: "johnpork@gmail.com"}
 
-            return user;
-        }
-    })
-  ],
-  secret : process.env.SECRET,
-  session:{
-    strategy:'jwt',
-  },
-  debug: process.env.NODE_ENV === "development"
-};
+//             return user;
+//         }
+//     })
+//   ],
+//   secret : process.env.SECRET,
+//   session:{
+//     strategy:'jwt',
+//   },
+//   debug: process.env.NODE_ENV === "development"
+// };
 
-const handler = NextAuth(authOptions)
+// const handler = NextAuth(authOptions)
 
-export {handler as GET, handler as POST}
+// export {handler as GET, handler as POST}
