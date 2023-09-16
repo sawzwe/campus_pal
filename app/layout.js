0d26815409e1e8e69fadea30d7370f57e39e0db2
sawzwe26 <sawjohnwaiyan@gmail.com>
2023-09-16 12:28:41 +0700

    favicon fixes

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,30 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import NavBar from './components/NavBar/page'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Tutor Plus',
   description: 'Improve your learning curve',
+  icons:{
+    icon: [
+    '/Logo/favicon.ico?v=4',
+  ],
+  apple: [
+    '/Logo/apple-touch-icon.png?v=4',
+  ],
+  shortcut: [
+    '/Logo/apple-touch-icon.png']
+},
+manifest: '/Logo/site.webmanifest'
+
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/Logo.png" type="image/svg+xml" />
       <body className={inter.className}>
         <NavBar />
         {children}
