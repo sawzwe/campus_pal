be0ee6b4fb435b8fb899d03894fecc196a9694fe
sawzwe26 <sawjohnwaiyan@gmail.com>
2023-09-16 12:16:56 +0700

    fixed bugs

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/Logo.png" type="image/svg+xml" />
       <body className={inter.className}>
-        <NavBar/>
+        <NavBar />
         {children}
       </body>
     </html>
