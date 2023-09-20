84ae471e3b1eab03ed8ac3472949ced90cfbcb09
sawzwe26 <sawjohnwaiyan@gmail.com>
2023-09-20 15:38:26 +0700

    auth bugs

--- app/page.js
+++ app/page.js
@@ -1,14 +1,10 @@
 import Landing from "/app/client/Landing/page.jsx";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route.jsx";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
   return (
     <>
       <Landing />
-      <h1>Server Side Rendered</h1>
-      <pre>{JSON.stringify(session)}</pre>
+
     </>
   );
 }
