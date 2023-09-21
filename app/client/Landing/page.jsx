6d56e137d975a3fca654af6a344dc88ce106df5a
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-09-21 15:16:37 +0700

    Fixed Link error.

--- app/client/Landing/page.jsx
+++ app/client/Landing/page.jsx
@@ -405,7 +405,7 @@ const Page = () => {
           severity="success"
         >
           You&apos;ve successfully registered for {selectedCourse?.title}.{" "}
-          <Link>Take me there</Link>.
+          <Link href="">Take me there</Link>.
         </MuiAlert>
       </Snackbar>
     </>
