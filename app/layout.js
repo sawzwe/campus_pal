e2e618ec36b3583cf9ad082c9a12f4d84e76a959
Akuju <94424420+sawzwe@users.noreply.github.com>
2023-12-07 15:36:24 +0700

    yolo

---
+++ app/layout.js
@@ -0,0 +1,32 @@
+import "./globals.css";
+import { Inter, Chakra_Petch } from "next/font/google";
+import NavBar from "./components/NavBar/page";
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
+
+const inter = Inter({ subsets: ["latin"] });
+const chakra = Chakra_Petch({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata = {
+  title: "Tutor Plus",
+  description: "Improve your learning curve",
+  icons: {
+    icon: ["/Logo/favicon.ico?v=4"],
+    apple: ["/Logo/apple-touch-icon.png?v=4"],
+    shortcut: ["/Logo/apple-touch-icon.png"],
+  },
+  manifest: "/Logo/site.webmanifest",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Landing from "/app/client/Landing/page.jsx";
+import Login from "/app/client//Login/page.jsx";
+
+export default async function Home() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
